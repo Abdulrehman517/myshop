3bace882a2c63a8c8c4e92d4af7ee58002d6c420
Abdulrehman517 <abdulrehman517uetian@gmail.com>
2021-12-28 14:29:06 +0500

    heroku deploye

--- myshop/settings.py
+++ myshop/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8m5t=enf1gyu4u&j2n3qn+vrcj(*fwgi69^r8h*a-0p_8$w++t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
